06128a9079e37b08a41d81a0ef52743d5003c563
minagawah <minagawa@tokyo800.jp>
2020-07-11 16:56:36 +0900

    feat: Adding my answers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rustlings-answers
-My answers to rustlings exercises.
+
+My answers to [rustlings](https://github.com/rust-lang/rustlings) exercises.
+I am a newbie to Rust language. Correct my codes if they are wrong.
---
+++ exercises/conversions/from_str.rs
@@ -0,0 +1,62 @@
+// This does practically the same thing that TryFrom<&str> does.
+// Additionally, upon implementing FromStr, you can use the `parse` method
+// on strings to generate an object of the implementor type.
+// You can read more about it at https://doc.rust-lang.org/std/str/trait.FromStr.html
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: usize,
+}
+
+// Steps:
+// 1. If the length of the provided string is 0, then return an error
+// 2. Split the given string on the commas present in it
+// 3. Extract the first element from the split operation and use it as the name
+// 4. Extract the other element from the split operation and parse it into a `usize` as the age
+// If while parsing the age, something goes wrong, then return an error
+// Otherwise, then return a Result of a Person object
+impl FromStr for Person {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() < 1 {
+            Err(String::from("Given string has no length"))
+        } else {
+            let pair = s.split(",").collect::<Vec<&str>>();
+            let name: &str = pair[0];
+            let age: usize = match pair[1].to_string().parse::<usize>() {
+                Ok(n) => { n },
+                Err(e) => { return Err(e.to_string()); }
+            };
+            Ok(Person {
+                name: name.to_string(),
+                age: age
+            })
+        }
+    }
+}
+
+fn main() {
+    let p = "Mark,20".parse::<Person>().unwrap();
+    println!("{:?}", p);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        assert!("".parse::<Person>().is_err());
+    }
+    #[test]
+    fn good_input() {
+        assert!("John,32".parse::<Person>().is_ok());
+    }
+    #[test]
+    #[should_panic]
+    fn missing_age() {
+        "John".parse::<Person>().unwrap();
+    }
+}
